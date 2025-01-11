80676a05b1fdb62bf3f353cf17c1124007f49bd7
An Nguyen <urbox@192.168.1.244>
2025-01-11 09:17:15 +0700

    ci: update 21

--- apps/nextjs-module-livestream/pages/index.tsx
+++ apps/nextjs-module-livestream/pages/index.tsx
@@ -2,7 +2,7 @@ const Page = () => {
   return (
     <div>
       <button type='button'>123123123123</button>
-      trigger20
+      trigger21
       {/* <ReactAppLoader title="2sfsfd" /> */}
     </div>
   );
